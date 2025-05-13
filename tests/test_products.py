37f1d9307eaced517b74bda4393815e1cfb09e84
Larissa Akemi <larissa.iuki@picpay.com>
2025-05-13 20:39:00 -0300

    update tests

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from product.models import Product
-from product.serializers import ProductSerializer
+from product.serializers.product_serializer import ProductSerializer
+from product.serializers.category_serializer import CategorySerializer
 
 class TestProduct(TestCase):
     def test_str_representation(self):
@@ -13,3 +14,21 @@ class TestProduct(TestCase):
         self.assertTrue(serializer.is_valid())
         self.assertEqual(serializer.validated_data["title"], "Livro Django")
         self.assertEqual(serializer.validated_data["price"], 100.0)
+
+class TestOrder(TestCase):
+    def test_order_serializer(self):
+        product_data = {"title": "Livro Django", "price": 100.0}
+        serializer = ProductSerializer(data=product_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data["title"], "Livro Django")
+        self.assertEqual(serializer.validated_data["price"], 100.0)
+
+class TestCategory(TestCase):
+    def test_category_serializer(self):
+        category_data = {"title": "Programming", "slug": "programming", "description": "Books about programming", "active": True}
+        serializer = CategorySerializer(data=category_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data["title"], "Programming")
+        self.assertEqual(serializer.validated_data["slug"], "programming")
+        self.assertEqual(serializer.validated_data["description"], "Books about programming")
+        self.assertTrue(serializer.validated_data["active"])
